523b1054c9fc1b84ea831e3789cb1b7f0a0587c9
ShashwatHTS <shashwat.pal@htstechsolutions.com>
2024-03-01 18:51:51 +0530

    done

--- src/get-users.ts
+++ src/get-users.ts
@@ -1,25 +1,26 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-  log: ["info","query",],});
+  log: ["info", "query"],
+});
 
 async function main() {
-    const users = await prisma.user.findMany({
-        where:{
-            email:"shashawat@123"
-        }
-     })
-    const user = await prisma.user.findUnique({
-        where:{
-            id:1
-        },
-        include:{
-            posts:true
-        }
-    })
-    console.log("users-------",users)
-    console.log("user-------",user)
-    // return user;
+  const users = await prisma.user.findMany({
+    where: {
+      email: "shashawat@123",
+    },
+  });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      posts: true,
+    },
+  })
+  console.log("users-------", users);
+  console.log("user-------", user);
+  // return user;
 }
 
 main()
